b5063ce9e402f4d0e81d3c9bbb64435087bd084d
Willow Barraco <willow.barraco@zol.fr>
2024-06-03 12:35:24 +0200

    prepare.sh: cleaning

--- script/prepare.sh
+++ script/prepare.sh
@@ -1,19 +1,24 @@
 #!/bin/sh -e
 
+html_dir="/var/www/html"
+
+www_folder() {
+	mkdir -p "$1"
+	chown -R www-data "$1"
+}
+
+www_file() {
+	touch "$1"
+	chown www-data "$1"
+}
+
 php_logs() {
-	conf_file="/usr/local/etc/php-fpm.d/www.conf"
+	www_folder "/var/log/php"
+	www_file "/var/log/php/access.log"
+	www_file "/var/log/php/error.log"
 
-	log_dir="/var/log/php"
-	access_log_file="${log_dir}/access.log"
-	error_log_file="${log_dir}/error.log"
-
-	mkdir -p "${log_dir}" \
-		&& touch "${access_log_file}" \
-		&& touch "${error_log_file}"
-
-	chown -R www-data "${log_dir}"
-
-	# The "c" supplementary letter is needed because the first letter will be cut at replacement.
+	# The "c" supplementary letter is needed because the first letter will be cut
+	# at replacement.
 	sed -i \
 		-e '/^;catch_workers_output/ccatch_workers_output = yes' \
 		-e '/^;log_level/clog_level = debug' \
@@ -23,15 +28,11 @@ php_logs() {
 		-e '/^;php_admin_value\[error_log\]/cphp_admin_value[error_log] = /var/log/php/error.log' \
 		-e '/^;php_admin_flag\[log_errors\]/cphp_admin_flag[log_errors] = on' \
 		-e '/^;clear_env/cclear_env = no' \
-		"${conf_file}"
+		"/usr/local/etc/php-fpm.d/www.conf"
 }
 
 symfony_logs() {
-	html_dir="/var/www/html"
-	var_dir="${html_dir}/var"
-
-	mkdir -p "${var_dir}"
-	chown -R www-data "${var_dir}"
+	www_folder "${html_dir}/var"
 }
 
 php_logs
